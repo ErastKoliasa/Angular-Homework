f6ca426511f94e6e5ac96955bc5c1309637027bf
Erast Koliasa <erast.koliasa@gmail.com>
2023-11-26 13:41:48 +0200

    Layout and styling of the products-list page

--- angular-homework/src/app/app.component.ts
+++ angular-homework/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ProductsListComponent } from './products/products-list/products-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ProductsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
