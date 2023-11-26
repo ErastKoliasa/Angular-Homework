07bde39b5ed000be22053b79cff5e97fd5410b92
Erast Koliasa <erast.koliasa@gmail.com>
2023-11-27 00:15:27 +0200

    Implemented the functionality of displaying data on the products list page

--- angular-homework/src/app/products/products-list/products-list.component.ts
+++ angular-homework/src/app/products/products-list/products-list.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
 import { Router } from '@angular/router';
+import { ProductsApiService } from '../../services/product-service/products-api.service';
+import { Observable } from 'rxjs';
+import { IProducts } from '../products';
+import { ProductsService } from '../../services/product-service/products.service';
 
 
 @Component({
@@ -13,8 +17,16 @@ import { Router } from '@angular/router';
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.css'
 })
-export class ProductsListComponent {
-  constructor(private router: Router){}
+export class ProductsListComponent implements OnInit{
+  public products$: Observable<IProducts[]> = this.productsService.products$;
+
+  constructor(private router: Router, private productsService: ProductsService, private productsApiService: ProductsApiService){}
+
+  ngOnInit(): void {
+    this.productsApiService.getProducts().subscribe(products => {
+      this.productsService.setPosts(products);
+    })
+  }
 
   goToAddNewProduct():void {
     this.router.navigate(["/addNewProduct"])
