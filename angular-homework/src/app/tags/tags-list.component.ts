f037c4a2e27990a4589ff19e6c4921ccb4748b1a
Erast Koliasa <erast.koliasa@gmail.com>
2023-11-26 17:35:12 +0200

    Move files to another folder

--- angular-homework/src/app/tags/tags-list.component.ts
+++ angular-homework/src/app/tags/tags-list.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-tags-list',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, MatFormFieldModule, MatInputModule],
   templateUrl: './tags-list.component.html',
   styleUrl: './tags-list.component.css'
 })
