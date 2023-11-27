2d41915f961ebc90a9905e45d089b7de9cc46e56
Erast Koliasa <erast.koliasa@gmail.com>
2023-11-27 11:09:26 +0200

    Implemented display of available tags in the "filter by" field

--- angular-homework/src/app/app.component.ts
+++ angular-homework/src/app/app.component.ts
@@ -5,12 +5,14 @@ import { MenuComponent } from './menu/menu.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ProductsApiService } from './services/product-service/products-api.service';
 import { ProductsService } from './services/product-service/products.service';
+import { TagService } from './services/tag-service/tag.service';
+import { TagsApiService } from './services/tag-service/tags-api.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, MenuComponent, HttpClientModule],
-  providers: [ProductsService,ProductsApiService],
+  providers: [TagService, TagsApiService, ProductsService, ProductsApiService,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- angular-homework/src/app/products/products-list/products-list.component.ts
+++ angular-homework/src/app/products/products-list/products-list.component.ts
@@ -8,25 +8,38 @@ import { ProductsApiService } from '../../services/product-service/products-api.
 import { Observable } from 'rxjs';
 import { IProducts } from '../products';
 import { ProductsService } from '../../services/product-service/products.service';
+import { TagService } from '../../services/tag-service/tag.service';
+import { ITags } from '../../tags/tags';
+import { FormsModule } from '@angular/forms';
+import { TagsApiService } from '../../services/tag-service/tags-api.service';
 
 
 @Component({
   selector: 'app-products-list',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatSelectModule, MatOptionModule, CurrencyPipe],
+  imports: [CommonModule, MatIconModule, MatSelectModule, MatOptionModule, CurrencyPipe, FormsModule],
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.css'
 })
 export class ProductsListComponent implements OnInit{
   public products$: Observable<IProducts[]> = this.productsService.products$;
+  public tags$: Observable<ITags[]> = this.tagsService.tags$;
+  public selectedTags:string[] = [];
 
-  constructor(private router: Router, private productsService: ProductsService, private productsApiService: ProductsApiService){}
+  constructor(private router: Router, 
+    private productsService: ProductsService, 
+    private productsApiService: ProductsApiService,
+    private tagsApiService: TagsApiService,
+    private tagsService: TagService){}
 
   ngOnInit(): void {
     this.productsApiService.getProducts().subscribe(products => {
       this.productsService.setPosts(products);
     })
-  }
+    this.tagsApiService.getTags().subscribe(tags => {
+      this.tagsService.setTags(tags)
+  })
+}
 
   goToAddNewProduct():void {
     this.router.navigate(["/addNewProduct"])
